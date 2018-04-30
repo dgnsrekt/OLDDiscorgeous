1f4a8633a0104ae0194b19efb7999218d01f9dc9
devtest <run2devtest@gmail.com>
2018-04-29 19:11:21 -0500

    init command opitions

--- discorgtts/cli.py
+++ discorgtts/cli.py
@@ -2,22 +2,42 @@ import click
 
 
 @click.group()
-def main():
+@click.option('-f', '--filename')  # XXX: <- add default that calls config mod
+@click.option('--bind_ip')  # XXX: <- add default that calls config mod
+@click.option('--bind_port')  # XXX: <- add default that calls config mod
+@click.pass_context
+def main(context, filename, bind_ip, bind_port):
+    context.obj = {
+        'FILENAME': filename,
+        'BIND_IP': bind_ip,
+        'BIND_PORT': bind_port
+    }
     pass
 
 
 @main.command('server')
-def server():
-    click.echo('running server')
+@click.pass_context
+def server(context):
+    # XXX: Args BIND_IP, BIND_PORT, FILE_NAME
+    # XXX: Accent -> Random, 'UK', 'US', 'AU'
+    # XXX: Discord -> Channel_id,
+    click.echo('erver')
+    print(context.obj)
 
 
 @main.command('client')
 def client():
+    # XXX:HOST_ADDRESS, USERNAME, PORT
     click.echo('running client')
 
 
 @main.command('sshclient')
-def server():
+def sshclient():
+    click.echo('running sshclient')
+
+
+@main.command('chat')
+def chatclient():
     click.echo('running sshclient')
 
 
