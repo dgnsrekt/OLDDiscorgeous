438f73134c3348961f1c6318c55342977672a255
devtest <run2devtest@gmail.com>
2018-05-04 12:39:12 -0500

    refactored for using new config structure

--- discorgtts/server_in.py
+++ discorgtts/server_in.py
@@ -1,8 +1,7 @@
-# TODO: Rename file DiscordGttsServerInput
-# TODO: Convert to ASYNCIO
-# https://docs.python.org/3/library/asyncio-protocol.html#udp-echo-server-protocol -> 18.5.4.3.2. TCP echo server protocol
 import asyncore
 import sys
+
+from config import Configuration
 from voice_message import VoiceMessageFile, VoiceMessageQueue
 from threading import Thread
 from time import sleep
@@ -19,17 +18,18 @@ class DiscordGttsHandler(asyncore.dispatcher_with_send):
         log.info(f'len: {len(data)}')
         if data:
             self.send(b'ACK!\n')
+            print(data)
             VoiceMessageQueue.push(data)
 
 
 class DiscordGttsServer(asyncore.dispatcher):  # TODO: Change to DiscordGttsInput
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, max_conn=10):
         asyncore.dispatcher.__init__(self)
         self.create_socket()
         self.set_reuse_addr()
         self.bind((host, port))
-        self.listen(10)  # add to config
+        self.listen(max_conn)  # add to config
         log.info(f'Listing on {host} {port}')
 
     def handle_accepted(self, sock, addr):
@@ -39,20 +39,19 @@ class DiscordGttsServer(asyncore.dispatcher):  # TODO: Change to DiscordGttsInpu
 
 class DiscordGttsServerInput:  # TODO: Change to DiscordGttsInputServer
 
-    def __init__(self):
-        self.server = DiscordGttsServer('localhost', 6666)
+    def __init__(self, ip, port, language, max_connections):
+        self.server = DiscordGttsServer(ip, port, max_connections)
         self.worker = Thread(target=self.start_voice_messsage_queue)  # TODO change this to async
         self.worker2 = Thread(target=asyncore.loop, name='Asyncore Loop')
-        self.stop_queue = False
+        self.language = language
 
     def start_voice_messsage_queue(self):
         vmf = VoiceMessageFile()
         while True:
             if not vmf.file_exists:
                 message = VoiceMessageQueue.pop()
-                vmf.create_message(message)  # logging here
+                vmf.create_message(message, self.language)  # logging here
             sleep(1)  # try .5 for speed
-        return
 
     def run(self):
         try:
@@ -67,7 +66,14 @@ class DiscordGttsServerInput:  # TODO: Change to DiscordGttsInputServer
 def main():
     sleep(1)
     print('running input server')
-    server = DiscordGttsServerInput()
+
+    config = Configuration()
+    bind_ip = config.server_settings['server_ip']
+    bind_port = int(config.server_settings['server_port'])
+    language = config.server_settings['language']
+    max_connections = int(config.server_settings['max_connections'])
+
+    server = DiscordGttsServerInput(bind_ip, bind_port, language, max_connections)
     server.run()
 
 
