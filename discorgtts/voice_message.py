f7cc0d2e158fa1934be47706fc68cf6512e2370e
devtest <run2devtest@gmail.com>
2018-05-04 12:40:23 -0500

    added docstrings and removed unused funcs

--- discorgtts/voice_message.py
+++ discorgtts/voice_message.py
@@ -10,35 +10,34 @@ class VoiceMessageFile:
     def __init__(self):
         self.filepath = Path(__file__).parent / 'message.mp3'
         self.file = str(self.filepath)
-        self.language = 'en-au'  # en-au, en-us, en-uk, random
+        # self.language = 'en-au'  # en-au, en-us, en-uk, random
 
-    @property
-    def language_setting():
-        pass
-
-    def create_message(self, message):
+    def create_message(self, message, language):
         msg = speech(text=message,
-                     lang=self.language,
+                     lang=language,
                      slow=False)
         msg.save(self.file)
 
     @property
     def duration(self):
+        '''returns the duration of the audio file in seconds'''
         if self.file_exists:
             return round(TinyTag.get(self.file).duration) + 1
         return None
 
     @property
     def mtime(self):
+        '''returns the most recent content modification expressed in seconds'''
         if self.file_exists:
             return self.filepath.stat().st_mtime
         return None
 
     @property
     def file_exists(self):
+        '''returns True if file exists'''
         return self.filepath.exists()
 
-    def delete(self):
+    def remove(self):
         if self.file_exists:
             sleep(1)
             self.filepath.unlink()
@@ -54,20 +53,17 @@ class VoiceMessageFile:
 
 
 class VoiceMessageQueue:
+    '''Stores messages waiting to be processed in a FIFO queue.'''
     _message_queue = Queue()
     # XXX: test out lifo, and lifo with a max
 
     def push(message):
+        '''Pushes new message to queue.'''
         VoiceMessageQueue._message_queue.put(message)
 
     def pop():
+        '''retuns True if the queue is empty.'''
         return VoiceMessageQueue._message_queue.get()
 
     def is_empty():
         return VoiceMessageQueue._message_queue.empty()
-
-
-x = VoiceMessageFile()
-print(x.mtime)
-print(x.duration)
-print(x)
